c9dc2521f54df433ddc783bbc1c81118c0a51e65
edom <edom.zhang@gmail.com>
2019-04-27 16:48:52 +1000

    Add id

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 //import "./App.css";
 import Card from "./Component/Card";
 import styled from "styled-components";
+// import { Card } from "antd";
 
 class App extends Component {
   state = {
@@ -10,28 +11,37 @@ class App extends Component {
         img:
           "https://pic4.zhimg.com/80/v2-2e21b2dcaf9233c5697c46524b2f4aae_qhd.jpg",
         title: "Cat",
-        subTitle: "Meo"
+        subTitle: "Meo",
+        id: 1
       },
       {
         img: "http://www.cup.com.hk/wp-content/uploads/2017/02/akita3.jpg",
         title: "Dog",
-        subTitle: "Woo"
+        subTitle: "Woo",
+        id: 2
       },
       {
         img:
           "https://gss3.bdstatic.com/-Po3dSag_xI4khGkpoWK1HF6hhy/baike/whfpf%3D280%2C150%2C0/sign=aac85eab9a2bd407429280bd1db4ac69/dc54564e9258d10910326931dc58ccbf6d814d64.jpg",
         title: "Pig",
-        subTitle: "hen"
+        subTitle: "hen",
+        id: 3
       }
     ]
   };
   render() {
+    //Destructing
     const { allcards } = this.state;
     return (
       <div className="App">
         <Container>
           {allcards.map(card => (
-            <Card img={card.img} title={card.title} subTitle={card.subTitle} />
+            <Card
+              img={card.img}
+              title={card.title}
+              subTitle={card.subTitle}
+              key={card.id}
+            />
           ))}
         </Container>
       </div>
@@ -44,5 +54,5 @@ export default App;
 const Container = styled.div`
   background-color: lightgrey;
   width: 600px;
-  heighth: 600px;
+  height: 600px;
 `;
